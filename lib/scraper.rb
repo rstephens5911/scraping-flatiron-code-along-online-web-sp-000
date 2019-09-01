48fa7058d3493e5b8a444d02bf8d1b69edf736d7
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-01 20:17:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,10 @@ class Scraper
   end
 
   def get_courses
-
+    courses = doc.css(".post")
+    title = doc.css(".post").first.css(".h2").text
+    schedule = doc.css(".post").first.css(".date").text
+    description = doc.css(".post").first.css("p").text
 
 
   end
