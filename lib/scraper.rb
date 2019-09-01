dca884b14521e78bca8ff9e2b8989c3983bdf3f2
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-01 20:23:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,6 @@ class Scraper
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css("date").text
-      course.date = post.css("date").text
       course.description = post.css("p").text
     end
     binding.pry
