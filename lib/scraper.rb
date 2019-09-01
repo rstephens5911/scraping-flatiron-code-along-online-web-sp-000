c7e9c945d979b933090932f2416fcc542a0d8093
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-01 19:51:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require_relative "./course.rb"
 class Scraper
   def get_page
     html = open( http://learn-co-curriculum.github.io/site-for-scraping/courses)
+    Nokogiri::HTML(html)
   end
 
 end
